e2b906356622ad63b45133a0cd5a107353582eed
adoniaspp <adoniaspp@gmail.com>
2019-11-23 23:02:50 -0300

    Atualização Fomantic ui

--- configuracao/Conexao.php
+++ configuracao/Conexao.php
@@ -11,8 +11,8 @@ class Conexao {
     public static function getInstance() {
 
         if (!isset(self::$instance)) {
-            self::$instance = new PDO('mysql:host=localhost;dbname=pip', 'root', '', 
-                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$instance = new PDO('mysql:host=sql214.main-hosting.eu;dbname=u644393490_pip', 'u644393490_usrbd', 'osestudantes1',
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         } return self::$instance;
